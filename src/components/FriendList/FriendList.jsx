d4d235e5234571d50aaa2f9f4fec25c2de3c9b74
Inna <inna.kornieko@gmail.com>
2022-09-09 18:11:36 +0300

    Add jsx file

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FriendListItem } from './FriendListItem';
+// import css from './FriendList.module.css';
+
+export const FriendList = ({ friends }) => {
+    return (
+       <ul>
+       {friends.map(({ avatar, name, isOnline, id }) => (
+       <FriendListItem 
+        key={id}
+        avatar={avatar}
+        name={name}
+        isOnline={isOnline}
+        ></FriendListItem>
+       ))}
+</ul>
+)}
+
+FriendList.propTypes = {
+    friends: PropTypes.arrayOf(
+        PropTypes.shape({
+            avatar: PropTypes.string.isRequired,
+            name: PropTypes.string.isRequired,
+            isOnline: PropTypes.bool.isRequired,
+            id: PropTypes.number.isRequired, 
+        }) 
+    )
+};
